33cf2fe2e966e4fe406a845fc5465434b05b0c5b
cauecrb <crburgardt@gmail.com>
2025-03-13 09:42:52 -0300

    fix collor tasks and favorite

--- app/Models/Tasks.php
+++ app/Models/Tasks.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tasks extends Model
 {
-    protected $fillable = ['title', 'description', 'due_date', 'completed'];
+    protected $fillable = [
+        'title', 
+        'description', 
+        'due_date', 
+        'completed', 
+        'is_favorite', 
+        'color'
+    ];
 }
